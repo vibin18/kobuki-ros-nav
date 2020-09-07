89e64850a26eba41ce3faf649be9931b819a15a9
vibin18 <vibin.manavalan@gmail.com>
2020-09-07 20:40:50 +0200

    wip

--- smach01.py
+++ smach01.py
@@ -15,14 +15,14 @@ smach.State.__init__(NavGoalPublish, outcomes=['success', 'failed'])
 
 stateMachine1 = smach.StateMachine(outcomes=['FAILED'])
 
-    with stateMachine1:
-        StateMachine.add('KITCHEN',
-            NavGoalPublish(3.8  0.55)),
-            transitions={'success': 'ENTRANCE', 'failed' : 'FAILED' })
+with stateMachine1:
+    StateMachine.add('KITCHEN',
+        NavGoalPublish(3.8  0.55)),
+        transitions={'success': 'ENTRANCE', 'failed' : 'FAILED' })
 
-        StateMachine.add('ENTRANCE',
-            NavGoalPublish(-0.577 , 0.151),
-            transitions={'success': 'KITCHEN', 'failed' : 'FAILED' })
+    StateMachine.add('ENTRANCE',
+        NavGoalPublish(-0.577 , 0.151),
+        transitions={'success': 'KITCHEN', 'failed' : 'FAILED' })
 
 sis = smach_ros.IntrospectionServer('smach_server', stateMachine1 , '/SM_ROOT')
 sis.start()
